87ebf52f22aa3ef93bb2f6f04cea45ddc8c9fb59
Atanas Kambitov <atanasskambitov@gmail.com>
2018-06-29 00:02:41 +0300

    Empty

--- WebServer/GameStoreApplication/Data/Connection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace HTTPServer.GameStoreApplication.Data
-{
-    public static class Connection
-    {
-        public const string ConnectionString = @"Data Source=HAL\MSSQLSERVER2;Database=GameStoreDb;Integrated Security=True";
-    }
-}
